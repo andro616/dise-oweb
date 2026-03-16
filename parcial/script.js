d9b7b7752e5e140a7315757ca889b19f9eca70c6
andro616 <davidpiamba23@gmail.com>
2026-03-16 10:23:53 -0500

    inicio de js

--- parcial/script.js
+++ parcial/script.js
@@ -1,2 +1,11 @@
-document.querySelector('button').addEventListener('click', btnCalcular );
+document.querySelector('button').addEventListener('click', Calcular );
+
+function calcular(){
+    const edad  = parseFloat(document.getElementById('edad').value);
+    const presion = parseFloat(document.getElementById('presion').value);
+    const colestero = parseFloat(document.getElementById('colestero').value);
+    const fumador = parseFloat(document.getElementById('fumador').value);
+
+
+}
 
